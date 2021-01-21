f4c3ef2187ad13c901824f47f702269c75a84f2e
matthew-gordon <m.gordon5525@gmail.com>
2021-01-20 21:29:09 -0700

    [WIP] add password hashing

--- app/server/src/db/seeds/01_users_seed.js
+++ app/server/src/db/seeds/01_users_seed.js
@@ -1,16 +1,25 @@
+const bcrypt = require('bcryptjs');
+
 exports.seed = function (knex) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync('password123', salt);
+
   // Deletes ALL existing entries
   return knex('users')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
-        {
-          id: 1,
-          username: 'new_user',
-          email: 'user@email.com',
-          password: 'password123',
-        },
-      ]);
+      return knex('users')
+        .insert([
+          {
+            id: 1,
+            username: 'new_user',
+            email: 'user@email.com',
+            password: hash,
+          },
+        ])
+        .then(() =>
+          knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))`)
+        );
     });
 };
