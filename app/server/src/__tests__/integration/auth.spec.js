91e448aaa04974ed9413069a43b312c9f67b0725
matthew-gordon <m.gordon5525@gmail.com>
2021-01-22 22:19:04 -0700

    [WIP] add register route and test

--- app/server/src/__tests__/integration/auth.spec.js
+++ app/server/src/__tests__/integration/auth.spec.js
@@ -60,4 +60,52 @@ describe('routes : auth', () => {
       expect(res.body.user).toHaveProperty('password');
     });
   });
+
+  describe('POST /auth/register', () => {
+    it('throws an error if username taken', async () => {
+      const res = await request.post('/auth/register').send({
+        email: 'new@email.com',
+        username: 'new_user',
+        password: 'password123',
+      });
+
+      expect(res.status).toBe(500);
+      expect(res.type).toBe('application/json');
+      expect(res.body.status).toBe('error');
+      expect(res.body.message).toBe('username taken');
+    });
+
+    it('throws an error if email taken', async () => {
+      const res = await request.post('/auth/register').send({
+        email: 'user@email.com',
+        username: 'new_user',
+        password: 'password123',
+      });
+
+      expect(res.status).toBe(500);
+      expect(res.type).toBe('application/json');
+      expect(res.body.status).toBe('error');
+      expect(res.body.message).toBe('username taken');
+    });
+
+    it('returns a valid user with token', async () => {
+      const res = await request.post('/auth/register').send({
+        email: 'new@email.com',
+        username: 'gordo_mateo',
+        password: 'password123',
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.type).toBe('application/json');
+      expect(res.body.status).toBe('success');
+      expect(res.body).toHaveProperty('token');
+      expect(res.body).toHaveProperty('user');
+      expect(res.body.user).toHaveProperty('id');
+      expect(res.body.user.id).toBe(2);
+      expect(res.body.user).toHaveProperty('username');
+      expect(res.body.user.username).toBe('gordo_mateo');
+      expect(res.body.user).toHaveProperty('email');
+      expect(res.body.user.email).toBe('new@email.com');
+    });
+  });
 });
--- app/server/src/routes/auth.js
+++ app/server/src/routes/auth.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import db from '../db';
 import { comparePass, createToken } from '../utils';
 
@@ -19,6 +20,48 @@ router.post('/auth/login', async (req, res) => {
     res.status(200).send({ status: 'success', user, token });
   } catch (err) {
     res.status(500).send({ status: 'error', message: err.message });
+  }
+});
+
+router.post('/auth/register', async (req, res) => {
+  try {
+    const username = await db('users')
+      .where({ username: req.body.username })
+      .first();
+
+    if (username) {
+      throw new Error('username taken');
+    }
+
+    const email = await db('users').where({ email: req.body.email }).first();
+
+    if (email) {
+      throw new Error('email taken');
+    }
+
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const [user] = await db('users')
+      .insert({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+      })
+      .returning(['id', 'username', 'email', 'created_at', 'updated_at']);
+
+    const token = await createToken(user);
+
+    res.status(200).send({
+      status: 'success',
+      token,
+      user,
+    });
+  } catch (err) {
+    res.status(500).send({
+      status: 'error',
+      message: err.message,
+    });
   }
 });
 
